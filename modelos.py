8a3fd7f8de091d8679cb98f7276f0cad742b7a27
Khay150 <fvanotti15@gmail.com>
2025-05-15 17:08:42 -0300

    Se agrega el item 7

--- modelos.py
+++ modelos.py
@@ -2,32 +2,33 @@ import cplex
 
 problem = cplex.Cplex()
 problem.set_problem_type(cplex.Cplex.problem_type.LP)
-problem.objective.set_sense(problem.objective.sense.maximize)
+problem.objective.set_sense(problem.objective.sense.minimize)
 
 # Variables
-problem.variables.add(names=["x1", "x2", "x3"], lb=[0, 0, 0])
+problem.variables.add(names=["y1", "y2", "y3", "y4", "y5"], lb=[0, 0, 0, 0, 0])
 
-# Función objetivo: 5.900 x1 + 2900 x2 + 1200 x3 - 13500000
-problem.objective.set_linear([("x1", 5900), ("x2", 2900), ("x3", 1200)])
-problem.objective.set_offset(-13500000)
+# Función objetivo: 5.900 x1 + 2900 x2 + 1200 x3 
+problem.objective.set_linear([("y1", 38000), ("y2", 80000), ("y3", 1000), ("y4", 3000), ("y5", 7000)])
+
 
 # Restricciones
 problem.linear_constraints.add(
     lin_expr=[
-        [["x1", "x2", "x3"], [10, 5, 3]],
-        [["x1", "x2", "x3"], [20, 10, 6]],
-        [["x1"], [1]],
-        [["x2"], [1]],
-        [["x3"], [1]]
+        [["y1", "y2", "y3"], [10, 20, 1]],
+        [["y1", "y2", "y4"], [5, 10, 1]],
+        [["y1", "y2", "y5"], [3, 6, 1]],
     ],
-    senses=["L", "L", "L", "L", "L"],
-    rhs=[38000, 80000, 1000, 3000, 7000]
+    senses=["G", "G", "G"],
+    rhs=[5900, 2900, 1200]
 )
 
 # Resolver
 problem.solve()
 
-print("x1 =", problem.solution.get_values("x1"))
-print("x2 =", problem.solution.get_values("x2"))
-print("x3 =", problem.solution.get_values("x3"))
+print("y1 =", problem.solution.get_values("y1"))
+print("y2 =", problem.solution.get_values("y2"))
+print("y3 =", problem.solution.get_values("y3"))
+print("y4 =", problem.solution.get_values("y4"))
+print("y5 =", problem.solution.get_values("y5"))
 print("Z =", problem.solution.get_objective_value())
+
