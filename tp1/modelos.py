2dada17b93537e9b33a686b749e620978f9b7e60
Khay150 <fvanotti15@gmail.com>
2025-06-18 13:49:21 -0300

    Se ordenan carpetas para cada tp

--- tp1/modelos.py
+++ tp1/modelos.py
@@ -5,30 +5,27 @@ problem.set_problem_type(cplex.Cplex.problem_type.LP)
 problem.objective.set_sense(problem.objective.sense.maximize)
 
 # Variables
-problem.variables.add(names=["x1", "x2", "x3"], lb=[0, 0, 0])
+problem.variables.add(names=["x1", "x2"], lb=[0, 0])
 
 # Función objetivo: 5.900 x1 + 2900 x2 + 1200 x3 
-problem.objective.set_linear([("x1", 5900), ("x2", 3100), ("x3", 1200)])
+problem.objective.set_linear([("x1", 7), ("x2", 6)])
 
 
 # Restricciones
 problem.linear_constraints.add(
-    lin_expr=[
-        [["x1", "x2", "x3"], [10, 5, 3]],
-        [["x1", "x2", "x3"], [20, 10, 6]],
-        [["x1"], [1]],
-        [["x2"], [1]],
-        [["x3"], [1]]
-    ],
-    senses=["L", "L", "L", "L", "L"],
-    rhs=[38000, 80000, 1000, 3000, 7000]
-)
+     lin_expr=[
+        [["x1", "x2"], [1, 3]],
+        [["x1", "x2"], [2, 1]],
+        [["x1", "x2"], [2, 3]]
+        
+     ],
+     senses=["L", "L", "L"],
+     rhs=[6, 9, 10]
+ )
 
 # Resolver
 problem.solve()
 
 print("x1 =", problem.solution.get_values("x1"))
 print("x2 =", problem.solution.get_values("x2"))
-print("x3 =", problem.solution.get_values("x3"))
 print("Z =", problem.solution.get_objective_value())
-
