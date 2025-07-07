e78f93df8579e3c2177c570905f7bd1dd8f39d59
Ivo Lamonica <ivolamonicam@gmail.com>
2025-07-07 00:49:40 -0300

    cambio

--- tp3/Codigo/funciones.py
+++ tp3/Codigo/funciones.py
@@ -18,7 +18,7 @@ def crear_instancia(nombre_archivo="instancia.txt", n_dim=2, n_puntos=10,
         # Crear una grilla uniforme dentro del rango
         lado = int(np.ceil(n_puntos ** (1/n_dim)))
         grids = [np.linspace(rango_coords[0], rango_coords[1], lado) for _ in range(n_dim)]
-        malla = np.meshgrid(*grids)
+        malla = np.meshgrid(*grids) #Esto admite hasta dimensión 32 (por eso las pruebas no pasan de esa dimensión)
         puntos = np.stack([m.flatten() for m in malla], axis=-1)[:n_puntos]
         
     elif distribucion == "clusters":
